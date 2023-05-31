5fab5a763533fbd94b3b56f7030a27f0208031c6
tommy <tarunsuresh.v@gmail.com>
2023-05-31 09:36:49 +0000

    Complete Header (Responsive)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,22 @@
-import {Link} from 'react-router-dom'
+import {Link, withRouter} from 'react-router-dom'
+import {AiFillHome} from 'react-icons/ai'
+import {FiLogOut} from 'react-icons/fi'
+import {BsFillBriefcaseFill} from 'react-icons/bs'
 import Cookies from 'js-cookie'
 
 import './index.css'
 
-const Header = () => {
+const Header = props => {
   const onClickLogout = () => {
+    const {history} = props
+
     Cookies.remove('jwt_token')
+
+    history.replace('/')
   }
 
-  return (
-    <nav className="header-container">
-      <img
-        className="nav-logo"
-        src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
-        alt="website logo"
-      />
-
+  const renderDesktopView = () => (
+    <>
       <ul className="nav-list">
         <li className="nav-item">
           <Link className="nav-link" to="/">
@@ -33,8 +34,46 @@ const Header = () => {
       <button className="logout-btn" type="button" onClick={onClickLogout}>
         Logout
       </button>
+    </>
+  )
+
+  const renderMobileView = () => (
+    <ul className="nav-icon-container">
+      <li className="nav-item">
+        <Link className="nav-link" to="/">
+          <AiFillHome size={25} />
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/jobs">
+          <BsFillBriefcaseFill size={25} />
+        </Link>
+      </li>
+      <li className="nav-item">
+        <button
+          className="logout-btn-mobile"
+          type="button"
+          onClick={onClickLogout}
+        >
+          <FiLogOut size={25} color="#ffffff" />
+        </button>
+      </li>
+    </ul>
+  )
+
+  return (
+    <nav className="header-container">
+      <Link className="nav-link" to="/">
+        <img
+          className="nav-logo"
+          src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+          alt="website logo"
+        />
+      </Link>
+      {renderDesktopView()}
+      {renderMobileView()}
     </nav>
   )
 }
 
-export default Header
+export default withRouter(Header)

8f8556407cc076b49dfab409066f7af444a345c7
tommy <tarunsuresh.v@gmail.com>
2023-06-02 09:03:25 +0000

    Fix Find Jobs Button

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,30 +1,36 @@
-import {Component} from 'react'
-
 import Header from '../Header'
 
 import './index.css'
 
-class Home extends Component {
-  render() {
-    return (
-      <>
-        <Header />
-        <div className="home-bg-container">
-          <div className="content-container">
-            <h1 className="home-heading">Find The Job That Fits Your Life</h1>
-            <p className="home-description">
-              Millions of people are searching for jobs, salary information,
-              company reviews find your job that fits your abilities and
-              potential.
-            </p>
-            <button className="find-jobs-btn" type="button">
-              Find Jobs
-            </button>
-          </div>
-        </div>
-      </>
-    )
+const Home = props => {
+  const {history} = props
+
+  const onClickFindJobs = () => {
+    history.push('/jobs')
   }
+
+  return (
+    <>
+      <Header />
+      <div className="home-bg-container">
+        <div className="content-container">
+          <h1 className="home-heading">Find The Job That Fits Your Life</h1>
+          <p className="home-description">
+            Millions of people are searching for jobs, salary information,
+            company reviews find your job that fits your abilities and
+            potential.
+          </p>
+          <button
+            className="find-jobs-btn"
+            type="button"
+            onClick={onClickFindJobs}
+          >
+            Find Jobs
+          </button>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Home
